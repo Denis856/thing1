d2f7970f552850a0775bbe9c20e8e9f5f5ee40f6
Den89 <malyhindenis8@gmail.com>
2023-07-12 17:18:46 +0300

    lloop

--- js/script.js
+++ js/script.js
@@ -37,3 +37,20 @@ title01.forEach(item => {
       parent.querySelector('.footer__block').classList.toggle('active');
    });
 });
+
+// smooth move
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+   anchor.addEventListener('click', function (e) {
+      e.preventDefault()
+      
+      const blockID = anchor.getAttribute('href').substr(1)
+      
+      document.getElementById(blockID).scrollIntoView({
+         behavior: 'smooth',
+         block: 'start'
+      });
+   });
+}
